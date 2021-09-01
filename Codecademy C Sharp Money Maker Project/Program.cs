30c84522f49e26e7a00c3dd64391574917932a5e
Thomas-474 <teg2004@icloud.com>
2021-09-01 10:43:11 -0400

    project

---
+++ Codecademy C Sharp Money Maker Project/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecademy_C_Sharp_Money_Maker_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Enter amount of cents: ");
+
+            string centsAsString = Console.ReadLine();
+            double centsAsDouble = Convert.ToDouble(centsAsString);
+
+            Console.WriteLine($"{centsAsDouble} cents is equal to...");
+
+            int goldValue = 10;
+            int silverValue = 5;
+
+            double goldCoins = Math.Floor(centsAsDouble / goldValue);
+            double leftOverCentsAfterGold = centsAsDouble % goldValue;
+
+            double silverCoins = Math.Floor(leftOverCentsAfterGold / silverValue);
+            double bronzeCoins = leftOverCentsAfterGold % silverValue;
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {bronzeCoins}");
+
+            Console.ReadKey();
+        }
+    }
+}
